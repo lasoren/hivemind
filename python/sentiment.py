6e9c5a4d8232f097c4f2d242bbcb866bc486c6c2
Luke Sorenson <lasoren@me.com>
2014-10-04 17:01:25 -0400

    fix

--- python/sentiment.py
+++ python/sentiment.py
@@ -1,7 +1,7 @@
 import urllib.request
 import utils
 
-ef2c248e-a32d-47d2-836f-692976c55ead
+IDOL_API_KEY = "ef2c248e-a32d-47d2-836f-692976c55ead"
 
 GOOGLE_NEWS_RSS = "https://news.google.com/news/feeds?output=rss&q="
 SPACE = "%20"
