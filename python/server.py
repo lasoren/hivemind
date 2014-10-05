2027008f8d0820275e5b35377dee184fe024395d
Luke Sorenson <lasoren@me.com>
2014-10-04 22:46:08 -0400

    integrating sentiment into server

--- python/server.py
+++ python/server.py
@@ -1,19 +1,57 @@
 from flask import Flask
 from flask import Response
+from flask import request
+
+from multiprocessing.pool import ThreadPool
+import requests
+import utils
+
+GOOGLE_NEWS_RSS = "https://news.google.com/news/feeds?output=rss&q="
+SPACE = "%20"
+query = "iphone"
+
 app = Flask(__name__)
 
-@app.route("/sentiment")
-def sentiment():
-    return flask.jsonify(sentiment=0.5)
+@app.route('/articles', methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        query = request.form['query']
+        query = query.replace(" ", "%20")
+        url = GOOGLE_NEWS_RSS+query
+        response = requests.get(url).text
 
-@app.route("/articles")
-def articles():
-    json = {"sentiment" : 0.5,
-            "articles" : [
-                {"title": "Obama Eats Children",
-                 "snippet": "Obama seen eating children."}
-                ]}
-    return Response(json.dump(json), mimetype='application/json')
+        # Get article links from the RSS
+        links = []
+        utils.find_links(links, response)
+        links = links[2:]
+
+        # Get article titles from the RSS
+        titles = []
+        utils.find_titles(titles, response)
+
+        num_links = 5
+        pool = ThreadPool(processes=num_links)
+        articles = pool.map(utils.get_article_sentiment, links[:num_links])
+        # for i in range(num_links):
+        # utils.get_article_sentiment(links[0])
+
+        sentiments = []
+
+        result = {}
+        result["articles"] = []
+        for i in range(num_links):
+            info = {}
+            info["title"] = titles[i]
+            info["link"] = links[i]
+            sentiments.append(articles[i]["sentiment"])
+            info["sentiment"] = sentiments[i]
+            info["snippet"] = articles[i]["snippet"]
+            result["articles"].append(info)
+
+        average_sentiment = utils.average_sentiment(sentiments, num_links)  
+        result["sentiment"] = average_sentiment
+    return Response(json.dump(result), mimetype='application/json')
 
 if __name__ == "__main__":
     app.run()
