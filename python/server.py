644f72626d133c68a67d44b735e37b1e05c0b67b
Luke Sorenson <lasoren@me.com>
2014-10-04 23:39:38 -0400

    ugh

--- python/server.py
+++ python/server.py
@@ -15,8 +15,6 @@ app = Flask(__name__)
 @app.route('/api/articles', methods=['POST'])
 def articles():
     error = None
-    if not hasattr(threading.current_process(), "_children"):
-        threading.current_process()._children = weakref.WeakKeyDictionary()
     if request.method == 'POST':
         query = request.form['query']
         query = query.replace(" ", "%20")
