84b18709f6ecc79ad2054b90d14df75ae02984cf
Brandon Sprague <brandon00sprague@gmail.com>
2014-10-04 21:55:01 -0400

    Flask server

---
+++ python/flask.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import Response
+app = Flask(__name__)
+
+@app.route("/sentiment")
+def sentiment():
+    return flask.jsonify(sentiment=0.5)
+
+@app.route("/articles")
+def articles():
+    json = {"sentiment" : 0.5,
+            "articles" : [
+                {"title": "Obama Eats Children",
+                 "snippet": "Obama seen eating children."}
+                ]}
+    return Response(json.dump(json), mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run()
