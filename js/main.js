4cb7958d725c3eb02d81061235f0c551ba8960bf
Brandon Sprague <brandon00sprague@gmail.com>
2014-10-05 02:53:17 -0400

    UI changes, entities

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@ var fakeArticles = {
     {title: "Sharknado Approaches",
      snippet: "A Sharknado was spotted off the coast of Uzebekastan"}
   ]
-}
+};
+
+var fakeEntities = {
+  entities: ["Muffins", "Al Sharpton", "Twitter", "DDR"]
+};
 
 $(function(){
   $('.body').removeClass("hidden");
@@ -17,9 +21,14 @@ $(function(){
   
   // Swarm it up
   $('.swarm').click(function() {
+    $('.articles').animate({opacity: 0}, 500, function() {
+      $('.articles-holder').empty();
+    });
     var query = $('.query').val();
     // Make the actual API call
+    $('.loading').animate({opacity: 1}, 500);
     $.post('http://' + url + '/api/articles', {query: query}, function (data) {
+      //data = fakeArticles;
       $('.analysis').css({opacity: 0})
                     .removeClass("hidden")
                     .animate({opacity: 1}, 500, function() {
@@ -29,25 +38,34 @@ $(function(){
       $('.articles').css({opacity: 0})
                     .removeClass("hidden")
                     .animate({opacity: 1}, 500, function() {
+                      $('.loading').animate({opacity: 0}, 500);
                       animateArticles();
                     });
+   }, 'json');
+    $.post('http://' + url + '/api/entities', {query: query}, function (data) {
+        //data = fakeEntities;
+        drawEntities(data.entities);
     }, 'json');
   });
 });
 
 function drawPercentage(percentage) {
-  $('.progress-bar-danger').css({width: (100-percentage) + '%'});
-
-  $('.progress-bar-danger').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
-    $('.progress-bar-success').css({width: percentage + '%'});
-  });
+  $('.progress-bar-success').css({width: percentage + '%'});
 
   $('.progress-bar-success').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+    $('.progress-bar-danger').css({width: (100-percentage) + '%'});
+  });
+
+  $('.progress-bar-danger').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
     var red = Math.round((100-percentage)*255/100);
     var green = Math.round(percentage*255/100);
-    $('.sentiment-like').removeClass('invisible')
-                        .addClass('animated bounceIn')
-                        .text(Math.round(percentage) + '%');
+    var sentiment = $('.sentiment-like').text(Math.round(percentage) + '%');
+    var success_bar = $('.progress-bar-success');
+
+    sentiment.css({'margin-left': success_bar.width() + 'px'})
+             .removeClass('invisible')
+             .addClass('animated bounceIn');
+                        
   });
 }
 
@@ -55,13 +73,34 @@ function drawArticles(articles) {
   var holder = $('.articles-holder');
   for (var i = 0; i < articles.length; i++) {
     var article = articles[i];
-    holder.append('<h1 class="invisible">' + article.title + '</h1>');
-    holder.append('<p class="snippet lead invisible">' + article.snippet + '</p>');
+    var div =
+      '<div class="row item">' + 
+      '<div class="col-xs-1">';
+    if (article.sentiment >= 0.5) {
+      div += '<img src="img/like.png" class="img-responsive svg-img-big" alt="Dislike">'
+    } else {
+      div += '<img src="img/dislike.png" class="img-responsive svg-img-big" alt="Dislike">'
+    }
+    div += '</div>' +
+           '<div class="col-xs-11">' +
+           '<h2 class="invisible">' + article.title + '</h2>' +
+           '<p class="snippet lead invisible">' + article.snippet + '</p>' +
+           '</div>' +
+           '</div>'
+    holder.append(div);
+  }
+}
+
+function drawEntities(entities) {
+  var holder = $('.related .list-group');
+  for (var i = 0; i < entities.length; i++) {
+    var entity = entities[i];
+    holder.append('<a href="#" class="list-group-item">' + entity + '</a>');
   }
 }
 
 function animateArticles() {
-  $('.articles-holder > *').removeClass("invisible").addClass("animated fadeInDown");
+  $('.articles-holder .invisible').removeClass("invisible").addClass("animated fadeInDown");
 }
 
 function getParameterByName(name) {
