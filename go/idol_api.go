01a9526022f5ef939b96d27f6fc38bd065b8882d
Brandon Sprague <brandon00sprague@gmail.com>
2014-10-07 09:55:49 -0400

    A little more structure
    And removing some unnecessary files, and starting the Golang version

---
+++ go/idol_api.go
@@ -0,0 +1,8 @@
+package idol
+
+const (
+	ExtractEntities = "https://api.idolondemand.com/1/api/sync/extractentities/v1"
+	TokenizeText    = "https://api.idolondemand.com/1/api/sync/tokenizetext/v1"
+	GetSentiment    = "https://api.idolondemand.com/1/api/sync/detectsentiment/v1"
+	ApiKey          = "ef2c248e-a32d-47d2-836f-692976c55ead"
+)
